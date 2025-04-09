52c50e9625a5c50315b09ac7957239bb6e773d5d
DoanJackson <anhnonono0@gmail.com>
2025-04-09 16:57:47 +0700

    home page

--- src/app/(main)/index.tsx
+++ src/app/(main)/index.tsx
@@ -1,8 +1,8 @@
 import RecordsList from "@/components/RecordList";
 import { HOME_COLOR } from "@/utils/constant";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
+import { View, Text, StyleSheet, Image, ImageBackground, Dimensions } from "react-native";
+const {width, height} = Dimensions.get("window");
 const HomePage = () => {
   const records = [
     {
@@ -34,11 +34,40 @@ const HomePage = () => {
   ];
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, { paddingVertical: 180 }]}>
+      {/* <Text style={[styles.text, { paddingVertical: 180 }]}>
         Welcome to the Homepage!
-      </Text>
+      </Text> */}
+      {/* <View style={{width: "100%" , height: 300, position: "relative"}}>
+        <View style={{width: "100%" , height: 300, position: "absolute", marginBottom: 20}}>
+          <Image 
+          source={require("@/assets/images/home/home_header.png")}
+          resizeMode="contain"
+          style={{
+            height: "100%",
+            padding: 20,
+            justifyContent: 'center',
+            width: '100%',
+            resizeMode: 'cover'
+          }}
+          />
+        </View> */}
+        {/* <Text style={styles.greeting}> Hello there, Hung Jonathan </Text>
+      </View> */}
+      <ImageBackground 
+        source={require("@/assets/images/home/home_header.png")}
+        style={styles.background}
+      >
+        <View style={{flexDirection: "row", justifyContent: "space-between"}}>
+          <View>
+            <Text style={styles.greeting}> Hello there</Text>
+            <Text style={styles.quote}> Hung Jonathan </Text>
+          </View>
+          <View>
+            {/* <Image></Image> */}
+          </View>
+        </View>
+      </ImageBackground>
       <RecordsList records={records} />
-      {/* Add more components or content here */}
       {/* Header */}
       {/* MoodInput */}
       {/* RecentRecords */}
@@ -54,10 +83,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: HOME_COLOR.HOMEBACKGROUND,
   },
-  text: {
+  background: {
+    height: height * 0.4,
+    width: width,
+    padding: height * 0.05,
+    marginBottom: height * 0.03,
+  },
+  greeting: {
+    fontSize: 16,
+    color: "#000",
+  },
+  quote: {
     fontSize: 20,
     fontWeight: "bold",
-  },
+    color: '#000',
+  }
 });
 
 export default HomePage;
